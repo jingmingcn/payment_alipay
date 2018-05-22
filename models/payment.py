ef58962b9da6824d61b05acd3473965441ed4b16
Ming Jing <jingming.cn@gmail.com>
2018-05-23 03:22:43 +0800

    _

--- models/payment.py
+++ models/payment.py
@@ -107,7 +107,7 @@ class AcquirerAlipay(models.Model):
         #biz_content['body'] = '%s: %s' % (self.company_id.name, values['reference'])
 
         _logger.info('biz_content : %s'%(json.dumps(biz_content)))
-        biz_content_sign = func.rsaSign(json.dumps(biz_content),self.alipay_private_key)
+        biz_content_sign = func.rsaSign(json.dumps(biz_content),self.alipay_public_key)
 
         alipay_tx_values.update({'biz_content':biz_content_sign.decode('utf-8')})
         
